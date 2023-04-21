ec17efa6e49007d7dd6b8cee84f3b8423640033a
Yahya9102 <112933451+Yahya9102@users.noreply.github.com>
2023-04-21 08:00:38 +0200

    Cors didn´t work..

--- .history/pages/api/index_20230421075757.ts
+++ .history/pages/api/index_20230421075757.ts
@@ -35,7 +35,7 @@ export default async function handler(
       default: {
         // Return a 405 Method Not Allowed error for all other HTTP methods
         //res.setHeader("Access-Control-Allow-Origin", "*")
-        res.setHeader("Allow", "GET")
+        res.setHeader("Allow", "GET", "Access-Control-Allow-Origin", "*")
         res.status(405).end(`Method ${req.method} Not Allowed`)
         break
       }
