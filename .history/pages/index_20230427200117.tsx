91d9696508caf9fd50b329622c9c77fa084d04c3
Yahya9102 <112933451+Yahya9102@users.noreply.github.com>
2023-04-27 20:01:36 +0200

    vercel bug fix mob

---
+++ .history/pages/index_20230427200117.tsx
@@ -0,0 +1,25 @@
+import { NextPage } from "next"
+import LoadingPage from "@/p-components/loadingPage"
+
+const startPage: NextPage = ({}) => {
+  return (
+    <div>
+      <LoadingPage />
+    </div>
+  )
+}
+
+export default startPage
+
+// import { NextPage } from "next"
+// import LoadingPage from "@/p-components/loadingPage"
+
+// const startPage: NextPage = ({}) => {
+//   return (
+//     <div>
+//       <LoadingPage />
+//     </div>
+//   )
+// }
+
+// export default startPage
