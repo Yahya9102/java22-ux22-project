cf784d2d54b45509a8d075feb9f2ddd73744af1a
Kevin Åhlin <kevin.ahlin@hotmail.com>
2023-05-02 13:35:34 +0200

    inserted logo which takes you to Posts

--- p-components/header.tsx
+++ p-components/header.tsx
@@ -1,18 +1,29 @@
 import { NextPage } from "next"
 import Image from "next/image"
+import gmlogo from "/public/gmlogo.png"
 import styles from "./styles/header.module.css"
+import { useRouter } from "next/router"
 // import "../media/hamburger-menu.png"
 
 interface Props {}
 
 const Header: NextPage<Props> = ({}) => {
+  const router = useRouter()
+
+  const handleReturnToStartPage = () => {
+    router.push("/startPage")
+  }
+
   return (
     <div className={styles.header_background}>
       <header className={styles.header_body}>
         <div className={styles.menu_title}>
-          <h1>
+          <button type="submit" onClick={handleReturnToStartPage}>
+            <Image src={gmlogo} alt="Picture is unavailable" width={95} />
+          </button>
+          {/* <h1>
             <strong>GameMatch</strong>
-          </h1>
+          </h1> */}
         </div>
       </header>
     </div>
