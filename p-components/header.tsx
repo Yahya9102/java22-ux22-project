51b8df1c7b3de1fc236c7a6db2fc7c145a696d06
Kevin Åhlin <kevin.ahlin@hotmail.com>
2023-05-08 23:36:05 +0200

    fixed the hamburger menu navbar

--- p-components/header.tsx
+++ p-components/header.tsx
@@ -1,9 +1,14 @@
 import { NextPage } from "next"
 import Image from "next/image"
 import gmlogo from "/public/gmlogo.png"
+import Navposticon from "@/public/navtoposticon.png"
+import Adminlockicon from "@/public/adminlockicon.png"
+import Guidelinesicon from "@/public/guidelinesicon.png"
+import Closeicon from "@/public/xcloseicon.png"
 import styles from "./styles/header.module.css"
 import { useRouter } from "next/router"
-// import "../media/hamburger-menu.png"
+import { useState } from "react"
+import Burgermenu from "@/public/hamburger-menu.png"
 
 interface Props {}
 
@@ -14,17 +19,95 @@ const Header: NextPage<Props> = ({}) => {
     router.push("/startPage")
   }
 
+  const handleReturnToGuidelines = () => {
+    router.push("/GuidelinePage")
+  }
+
+  const handleReturnToAdminPage = () => {
+    router.push("/admin")
+  }
+
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
+
+  // show and hide hamburger-menu
+  const handleToggle = () => {
+    setMenuIsOpen(!menuIsOpen)
+  }
+
   return (
     <div className={styles.header_background}>
       <header className={styles.header_body}>
-        <div className={styles.menu_title}>
-          <button type="submit" onClick={handleReturnToStartPage}>
-            <Image src={gmlogo} alt="Picture is unavailable" width={95} />
+        <div className={styles.menu_icon}>
+          <button
+            type="submit"
+            onClick={handleToggle}
+            className={styles.nav_toggle_button}
+          >
+            <Image
+              src={Burgermenu}
+              alt="Picture unavailable"
+              width={40}
+              className={styles.nav_logo}
+            />
           </button>
-          {/* <h1>
-            <strong>GameMatch</strong>
-          </h1> */}
         </div>
+        <div className={styles.logo_container}>
+          <button type="submit" onClick={handleReturnToStartPage}>
+            <Image src={gmlogo} alt="Picture unavailable" width={95} />
+          </button>
+        </div>
+        <div className={styles.spacer}></div>
+
+        {menuIsOpen && (
+          <nav className={styles.navigation_bar}>
+            <ul className={styles.navigation_links}>
+              {/* <div className={styles.menu_icon}>
+                <button
+                  type="submit"
+                  onClick={handleToggle}
+                  className={styles.nav_toggle_button}
+                >
+                  <Image
+                    src={Closeicon}
+                    alt="Picture unavailable"
+                    width={28}
+                    className={styles.nav_logo}
+                  />
+                </button>
+              </div> */}
+              <li className={styles.postsListItem}>
+                <h5>Posts</h5>
+                <button type="submit" onClick={handleReturnToStartPage}>
+                  <Image
+                    src={Navposticon}
+                    alt="Picture unavailable"
+                    width={100}
+                  />
+                </button>
+              </li>
+              <li className={styles.guidlinesListItem}>
+                <h5>Guidelines</h5>
+                <button type="submit" onClick={handleReturnToGuidelines}>
+                  <Image
+                    src={Guidelinesicon}
+                    alt="Picture unavailable"
+                    width={100}
+                  />
+                </button>
+              </li>
+              <li className={styles.adminListItem}>
+                <h5>Admin</h5>
+                <button type="submit" onClick={handleReturnToAdminPage}>
+                  <Image
+                    src={Adminlockicon}
+                    alt="Picture unavailable"
+                    width={100}
+                  />
+                </button>
+              </li>
+            </ul>
+          </nav>
+        )}
       </header>
     </div>
   )
